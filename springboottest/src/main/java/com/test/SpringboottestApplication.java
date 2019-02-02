5a86321e87f2afd47c7fa77152e45c2500e0dc3e
Admin <Admin@Admin-PC>
2019-02-02 20:31:58 +0530

    hello added

--- springboottest/src/main/java/com/test/SpringboottestApplication.java
+++ springboottest/src/main/java/com/test/SpringboottestApplication.java
@@ -2,13 +2,20 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+//public class SpringboottestApplication extends  SpringBootServletInitializer{
 public class SpringboottestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(SpringboottestApplication.class, args);
 	}
 
+	/*@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SpringboottestApplication.class);
+	 }*/
 }
 
